d857ffecd842e05f4379b859ad81002c2787c66f
rajjohan <rm8007716@email.com>
2022-09-20 02:08:05 -0700

    template

---
+++ app.js
@@ -0,0 +1,25 @@
+// data type 
+
+let name = "raj jphan"
+console.log(typeof(name));
+
+
+let num = 123
+console.log(typeof(num))
+
+let a = null
+console.log(typeof(a))
+
+let b = true
+console.log(b)
+
+let c = false
+console.log(c)
+
+let d = undefined
+console.log(d)
+
+let g;
+console.log(g)
+
+ 
